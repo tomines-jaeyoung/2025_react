4b077c9ebafd22f50649741a60321e85c7b9bfa9
tomines-jaeyoung <tominesss12@gmail.com>
2025-10-14 16:25:06 +0900

    9장 codition redering

--- src/chp09/closing_ex/Toolbar.jsx
+++ src/chp09/closing_ex/Toolbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./toolbar.css";
 
 function Toolbar(props){
-    const {isLoggeIn, onClickLogin, onClickLogout} = props;
+    const {isLoggedIn, onClickLogin, onClickLogout} = props;
 
     return(
         <div className={"wrapper"}>
-            {isLoggeIn && <span className={"greeting"}>환영합니다.</span>}
-            {isLoggeIn ? (<button onClick={onClickLogout}>로그아웃</button>) : (<button onClick={onClickLogin}>로그인</button>)}
+            {isLoggedIn && <span className={"greeting"}>환영합니다.</span>}
+            {isLoggedIn ? (<button onClick={onClickLogout}>로그아웃</button>) : (<button onClick={onClickLogin}>로그인</button>)}
         </div>
     )
 }
